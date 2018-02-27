8f5506b7bc16d0d587333979fc7f383f3895d278
paraisolorrayne <lorrayneparaisoc@gmail.com>
2018-02-27 11:24:56 -0300

    Added icons

--- PitchPerfect/Controller/ViewController.swift
+++ PitchPerfect/Controller/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	//Mark - IBOutlets
+	
+	@IBOutlet weak var recordButton: UIButton!
+	@IBOutlet weak var stopRecordingButton: UIButton!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
